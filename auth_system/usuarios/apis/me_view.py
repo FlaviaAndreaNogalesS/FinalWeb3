17acc2c2bf90d247d2e137a71d2759331fa752f7
FlaviaAndreaNogalesS <fandreans@gmail.com>
2025-06-23 16:19:02 -0400

    Reorganizado: backend/auth_system

---
+++ auth_system/usuarios/apis/me_view.py
@@ -0,0 +1,17 @@
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+class MeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            "id": user.id,
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "rol": user.rol,
+            "is_staff": user.is_staff
+        })
